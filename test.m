e5dca7ff382ae5490d0d5c19fca2c27c26474925
Martin Klacik <klacikmartin@protonmail.com>
2023-03-03 12:27:00 +0100

    uloha 3

--- test.m
+++ test.m
@@ -1,3 +1,22 @@
 clc;
+clear;
+close all;
 
-chromosomePP = [1,2; 2,2]
+max_iteration = 5;
+max_numgen = 500;
+population = 100;
+n = 10;
+Space = ones(2,n).*[-500;500];
+
+for interation = 1:max_iteration
+    pop = genrpop(population, Space);
+
+    for gen = 1:max_numgen
+        fitness = schwefel(population)
+        parents = selbest(pop, fitness, [2 2 2 1 1 1]);
+        kid = crosgrp(parents, floor(population/5));
+    end
+
+
+
+end
--- uloha3.m
+++ uloha3.m
@@ -2,14 +2,14 @@ clear;
 clc;
 close all;
 hold off;
-MAX_INVEST = 2.5e6;
+MAX_INVEST = 2.5;
 SPACE = ones(2, 5).*[0; MAX_INVEST];
-POP_SIZE = 150;
+POP_SIZE = 75;
 
 
-MAX_GENERATIONS = 500;
-MAX_ITERATIONS = 10;
-MUTATION_RATE = 0.75;
+MAX_GENERATIONS = 50;
+MAX_ITERATIONS = 5;
+MUTATION_RATE = 0.6;
 AMPS = 10.0 * ones(1, 5);
 axis([-1 MAX_GENERATIONS+1 4e5 8e5]);
 hold on;
@@ -19,8 +19,10 @@ ALL_BEST = 0;
 for i = 1:MAX_ITERATIONS
     pop = genrpop(POP_SIZE, SPACE);
     HIGHEST_INCOME = 0;
+
 for g = 1:MAX_GENERATIONS
-    fitnessValues = financeFitnessDeathPenalty(pop);
+%     fitnessValues = financeFitnessDeathPenalty(pop);
+%     fitnessValues = financeFitnessProportional(pop);
     fitnessValues = financeFitnessGradational(pop);
 
 
@@ -32,7 +34,7 @@ for g = 1:MAX_GENERATIONS
         BEST_DECISION = pop(minIndex, :);
     end
 
-    popParents = selbest(pop, fitnessValues, [2 2 2 1 1 1]*floor(POP_SIZE/50));
+    popParents = selbest(pop, fitnessValues, [1 1 1 1 1 1 1]);
     %popChild = crossov(popParents, 2, 0);
     popChild = crosgrp(popParents, floor(POP_SIZE/5));
     popChildMutated = muta(popChild, MUTATION_RATE, AMPS, SPACE);
@@ -58,19 +60,22 @@ end
     plot(plotValues);
 end
 
-fprintf("Best from all iterations %6.2f\nBest investment combination:", ALL_BEST);
-disp(ALL_BEST_GENE)
+fprintf("\nBest income from all iterations: %6.2f\nBest investment combination:\n", ALL_BEST);
+disp(ALL_BEST_GENE*1e6);
 
 
 
-function [Fit] = financeFitnessGradational(population)
-    MAX_INVEST = 1e7;
+function [Fit] = financeFitnessProportional(population)
+    MAX_INVEST = 10;
     penatlyCoef = 1;
     [popSize, geneSize] = size(population);
     Fit = zeros(1, popSize);
     for p = 1:popSize
-        chromosome = num2cell(population(p, :));
-        [x1, x2, x3, x4, x5] = chromosome{:};
+        x1 = (population(p, 1));
+        x2 = (population(p, 2));
+        x3 = (population(p, 3));
+        x4 = (population(p, 4));
+        x5 = (population(p, 5));
         chromosome = [x1, x2, x3, x4, x5];
 
         Fit(p) = -(0.04*x1+0.07*x2+0.11*x3+0.06*x4+0.05*x5);
@@ -79,8 +84,8 @@ function [Fit] = financeFitnessGradational(population)
             Fit(p) = Fit(p) + penatlyCoef*(sum(chromosome) + MAX_INVEST);
         end
 
-        if x1 + x2 > 2.5e6
-            Fit(p) = Fit(p) + penatlyCoef*((x1 + x2) - 2.5e6);
+        if x1 + x2 > 2.5
+            Fit(p) = Fit(p) + penatlyCoef*((x1 + x2) - 2.5);
         end
 
         if -x4 + x5 > 0
@@ -90,25 +95,65 @@ function [Fit] = financeFitnessGradational(population)
         if (0.5 * (-x1-x2+x3+x4-x5)) > 0
             Fit(p) = Fit(p) + penatlyCoef*(0.5 * (-x1-x2+x3+x4-x5));
         end
+
+        Fit(p) = Fit(p)*1e6;
     end
 end
 
 function [Fit] = financeFitnessDeathPenalty(population)
     POKUTA = 0;
-    MAX_INVEST = 1e7;
+    MAX_INVEST = 10;
     [popSize, geneSize] = size(population);
     Fit = zeros(1, popSize);
     for p = 1:popSize
         if sum(population(p)) > MAX_INVEST
             Fit(p) = POKUTA;
-        elseif population(p, 1) + population(p, 2) > 2.5e6
+        elseif population(p, 1) + population(p, 2) > 2.5
             Fit(p) = POKUTA;
         elseif -population(p, 4) +population(p, 5) > 0
             Fit(p) = POKUTA;
         elseif (0.5 * (-population(p, 1)-population(p, 2)+population(p, 3)+population(p, 4)-population(p, 5))) > 0
             Fit(p) = POKUTA;
         else
-            Fit(p) = -(0.04*population(p, 1)+0.07*population(p, 2)+0.11*population(p, 3)+0.06*population(p, 4)+0.05*population(p, 5));
+            Fit(p) = -(0.04*population(p, 1)+0.07*population(p, 2)+0.11*population(p, 3)+0.06*population(p, 4)+0.05*population(p, 5))*1e6;
         end
+    end
+end
+
+
+function [Fit] = financeFitnessGradational(population)
+    MAX_INVEST = 10;
+    [popSize, geneSize] = size(population);
+    Fit = zeros(1, popSize);
+    for p = 1:popSize
+        x1 = (population(p, 1));
+        x2 = (population(p, 2));
+        x3 = (population(p, 3));
+        x4 = (population(p, 4));
+        x5 = (population(p, 5));
+        chromosome = [x1, x2, x3, x4, x5];
+
+        Fit(p) = -(0.04*x1+0.07*x2+0.11*x3+0.06*x4+0.05*x5);
+
+        penaltyCount = 0;
+        penaltyBase = 850;
+
+        if sum(chromosome) > MAX_INVEST
+            penaltyCount = penaltyCount + 1;
+        end
+
+        if x1 + x2 > 2.5
+            penaltyCount = penaltyCount + 1;
+        end
+
+        if -x4 + x5 > 0
+            penaltyCount = penaltyCount + 1;
+        end
+         
+        if (0.5 * (-x1-x2+x3+x4-x5)) > 0
+            penaltyCount = penaltyCount + 1;
+        end
+
+        Fit(p) = Fit(p)*1e6 + penaltyBase^penaltyCount;
     end
 end
